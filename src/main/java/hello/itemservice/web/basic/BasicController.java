cbf62bf6392a856fbdf56b139a4e168263a387c3
tacotata <71251163+tacotata@users.noreply.github.com>
2022-06-12 17:51:17 +0900

    RedirectAttributes

--- src/main/java/hello/itemservice/web/basic/BasicController.java
+++ src/main/java/hello/itemservice/web/basic/BasicController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -89,11 +90,22 @@ public class BasicController {
         return "basic/item";
     }
 
-      @PostMapping("/add")
+      //@PostMapping("/add")
     public String addItemV5( Item item){
 
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        //쿼리파라미터로 넘어감
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
+
     }
 
     @GetMapping("/{itemId}/edit")
@@ -108,6 +120,8 @@ public class BasicController {
         itemRepository.update(itemId, item);
         return "redirect:/basic/items/{itemId}";
     }
+
+
 
     /**
      *테스트용 데이터 추가
